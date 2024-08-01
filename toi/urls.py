70fc85a992d21b373de5a15d859ebd23a805a80f
omdahalke4141 <omkumar.dahalke01@gmail.com>
2024-08-01 14:38:02 +0530

    asdf

--- toi/urls.py
+++ toi/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import *
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin_home'),  # Ensure this is unique
